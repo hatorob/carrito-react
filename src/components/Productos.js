1ff9583391377ff352021c21582ca1ec76155611
Hector Toro <alejotoro94@hotmail.com>
2022-08-07 23:14:05 -0500

    Dando estilos a los productos en component productos y producto con inline-style

--- src/components/Productos.js
+++ src/components/Productos.js
@@ -1,13 +1,21 @@
 import { Component } from "react";
 import Producto from './Producto';
 
+const styles = {
+    productos: {
+        display: 'flex', //para manipular más facil el contenido y centrar u orientar como deseamos
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    }
+}
+
 class Productos extends Component {
     render() {
         const { productos, agregarAlCarro } = this.props;
 
 
         return(
-            <div>
+            <div style={styles.productos}>
                 {productos.map(producto => 
                  <Producto
                     agregarAlCarro={agregarAlCarro}
